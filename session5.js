2c0cafe9a7bf4a8e09b18e2eaf4dd085e91b4e28
M-Mahjoob <mhymhj01@gmail.com>
2018-05-19 17:27:30 +0430

    first commit

---
+++ session5.js
@@ -0,0 +1,12 @@
+//// session 7
+//console.log("hello orod!");
+//var name = "Mahya";
+//console.log (name);
+//var age = 43;
+//console.log( age  +"  " +  name );
+//var lastname = prompt ("what is the lastname");
+//console.log ( "the lastname is "+ lastname);
+//
+//alert ("this is a alart page");
+//// the end of session 7
+//
